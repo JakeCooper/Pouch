7e00f761d5b779c10b6b4ec65310dd5e4e9ee6d8
Erik Reppel <erikreppel@gmail.com>
2017-09-15 23:27:00 -0700

    remove install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#xdg-mime install pouch.xml --novendor
-#sudo update-mime-database /usr/share/mime
-cp application-pouch.xml ~/.local/share/mime/packages/
-cp pouch.desktop ~/.local/share/applications/
-update-mime-database ~/.local/share/mime
-update-desktop-database ~/.local/share/applications
