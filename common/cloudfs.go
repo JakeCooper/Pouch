592d14cdb575edf228f38316dc17fcfaaf9d6f3d
Jake Cooper <jake.elijah.cooper@gmail.com>
2017-09-16 16:23:58 -0400

    Added watcher chan for adding new watchers

--- common/cloudfs.go
+++ common/cloudfs.go
@@ -1,5 +1,13 @@
 package common
 
+// WatcherObj : For adding/removing watchers on files
+type WatcherObj struct {
+	Path   string
+	Action string
+}
+
+var watcherChan = make(chan WatcherObj)
+
 // InitCloudFS returns an S3 interface
 func InitCloudFS(config *Configuration) (CloudStorage, error) {
 	return nil, nil
