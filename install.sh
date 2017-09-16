ceebc579f364bd1543755aa9bc3290d456e8c0bc
Jake Cooper <jake.elijah.cooper@gmail.com>
2017-09-16 00:16:54 -0400

    More Setup

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-xdg-mime install pouch.xml --novendor
-sudo update-mime-database /usr/share/mime
-
+#xdg-mime install pouch.xml --novendor
+#sudo update-mime-database /usr/share/mime
+cp pouch.xml ~/.local/share/mime/packages/
+cp pouch.desktop ~/.local/share/applications/
