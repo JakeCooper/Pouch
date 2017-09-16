456e058ddc48604c3f758ccab2f04213693037b7
Andrei Taylor <andreirtaylor@gmail.com>
2017-09-16 07:49:37 -0400

    MIME types are hard

--- register_mime_type.sh
+++ register_mime_type.sh
@@ -1,43 +1,33 @@
-#! bin/bash
+#! /bin/bash
 APP="Pouch"
 EXT="pouch"
 COMMENT="$APP's data file"
 
-# Create directories if missing
-mkdir -p ~/.local/share/mime/packages
-mkdir -p ~/.local/share/applications
-
-# Create mime xml 
-echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
-<mime-info xmlns=\"http://www.freedesktop.org/standards/shared-mime-info\">
-    <mime-type type=\"application/x-$APP\">
-        <comment>$COMMENT</comment>
-        <icon name=\"application-x-$APP\"/>
-        <glob pattern=\"*.$EXT\"/>
-    </mime-type>
-</mime-info>" > ~/.local/share/mime/packages/application-x-$APP.xml
+# Create mime xml (this doesnt work)
+#echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+#<mime-info xmlns=\"http://www.freedesktop.org/standards/shared-mime-info\">
+#    <mime-type type=\"application/x-$APP\">
+#        <comment>$COMMENT</comment>
+#        <icon name=\"application-x-$APP\"/>
+#        <glob pattern=\"*.$EXT\"/>
+#    </mime-type>
+#</mime-info>" > /usr/share/mime/application/x-$EXT.xml
 
 # Create application desktop
 echo "[Desktop Entry]
 Name=$APP
-Exec=/usr/bin/$APP %U
+Exec=Pouch %U
 MimeType=application/x-$APP
-Icon=$APP
+Icon=~/.local/share/icons/hicolor/48x48/apps/$EXT.png
 Terminal=false
-Type=Application
-Categories=
-Comment=
-"> ~/.local/share/applications/$APP.desktop
+Type=Application"> ~/.local/share/applications/$APP.desktop
+
+chmod 711 ~/.local/share/applications/$APP.desktop
+
+# copy associated icons to the icons folder
+# This also doesnt work........
+cp ./static/$EXT.png ~/.local/share/icons/hicolor/48x48/apps/
 
 # update databases for both application and mime
 update-desktop-database ~/.local/share/applications
 update-mime-database    ~/.local/share/mime
-
-# set default application. `mimeopen <filepath>` now opens things properly
-touch test.pouch
-echo "2\nPouch" | mimeopen -d test.pouch
-rm test.pouch
-
-# copy associated icons to pixmaps
-cp ./static/$APP.png                ~/.local/share/pixmaps
-cp ./static/application-x-$APP.png  ~/.local/share/pixmaps
