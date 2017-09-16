29827fddb2370849e37b949f150d0e6e06997ef8
Michael Reiter <michaelreiter@live.ca>
2017-09-16 08:49:20 -0400

    Add View skeleton

---
+++ front end/src/elm/View.elm
@@ -0,0 +1,12 @@
+module View exposing (view)
+
+import Html exposing (Html, div)
+import Messages exposing (Msg(..))
+import Model exposing (Model)
+
+
+view : Model -> Html Msg
+view model =
+    div
+        []
+        []
