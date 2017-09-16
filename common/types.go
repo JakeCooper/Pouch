5142482962d8bbb83206ed1715e5b6a4564d4660
Erik Reppel <erikreppel@gmail.com>
2017-09-16 05:18:50 -0700

    Remove unused struct

--- common/types.go
+++ common/types.go
@@ -1,8 +1,5 @@
 package common
 
-import "net/http"
-import "fmt"
-
 // CloudStorage : Interface for CloudStorage
 type CloudStorage interface {
 	// Create uploads a new file to remote storage
@@ -15,39 +12,4 @@ type CloudStorage interface {
 	Delete(path string) error
 	// List all files on remote fs
 	List() ([]string, error)
-}
-
-// HTTPStatus : Status for HTTP, just an int
-type HTTPStatus int
-
-// AWS : Struct for Amazon Web Services
-type AWS struct {
-	Creds string
-}
-
-// Create : Create for AWS S3
-func (aws AWS) Create(path string) HTTPStatus {
-	// myBucket, err := getS3Bucket(config.S3Root)
-	// checkAndFailure(err)
-	// myBucket.Put()
-	fmt.Println(path)
-	return http.StatusOK
-}
-
-// Read : Read for AWS S3
-func (aws AWS) Read(path string) HTTPStatus {
-	fmt.Println(path)
-	return http.StatusOK
-}
-
-// Update : Update for AWS S3
-func (aws AWS) Update(path string) HTTPStatus {
-	fmt.Println(path)
-	return http.StatusOK
-}
-
-// Delete : Delete for AWS S3
-func (aws AWS) Delete(path string) HTTPStatus {
-	fmt.Println(path)
-	return http.StatusOK
 }
