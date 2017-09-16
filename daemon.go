88887142086b8a4ce54d3c380465772f5c29e52a
Jake Cooper <jake.elijah.cooper@gmail.com>
2017-09-16 01:46:47 -0400

    Tumble filesystem events

--- daemon.go
+++ daemon.go
@@ -1,10 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/fsnotify/fsnotify"
 )
+
+func tumbleEvents(event fsnotify.Event) {
+	switch event.Op {
+	case fsnotify.Create:
+		fmt.Println("CREATED")
+	case fsnotify.Chmod:
+		fmt.Println("CHMOD")
+	case fsnotify.Remove:
+		fmt.Println("REMOVED")
+	case fsnotify.Rename:
+		fmt.Println("RENAME")
+	case fsnotify.Write:
+		fmt.Println("WRITE")
+	default:
+		fmt.Println("NONACTION DEFAULT")
+	}
+
+}
 
 func daemon() {
 	// Watch for file changes in root file
@@ -19,10 +38,7 @@ func daemon() {
 		for {
 			select {
 			case event := <-watcher.Events:
-				log.Println("event:", event)
-				if event.Op&fsnotify.Write == fsnotify.Write {
-					log.Println("modified file:", event.Name)
-				}
+				tumbleEvents(event)
 			case err := <-watcher.Errors:
 				log.Println("error:", err)
 			}
