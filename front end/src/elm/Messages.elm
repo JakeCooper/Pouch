a3db5e235627297a1d92bafe4a2d2e515c6d945a
Michael Reiter <michaelreiter@live.ca>
2017-09-16 14:42:59 -0400

    Add object sorting by name

--- front end/src/elm/Messages.elm
+++ front end/src/elm/Messages.elm
@@ -1,8 +1,9 @@
 module Messages exposing (Msg(..))
 
-import Model exposing (CloudObject)
+import Model exposing (CloudObject, Ordering)
 import RemoteData exposing (WebData)
 
 
 type Msg
     = OnFetchObjects (WebData (List CloudObject))
+    | OrderObjects Ordering
--- front end/src/elm/Update.elm
+++ front end/src/elm/Update.elm
@@ -1,7 +1,8 @@
 module Update exposing (update)
 
 import Messages exposing (Msg(..))
-import Model exposing (Model)
+import Model exposing (Model, Field(..), Order(..))
+import RemoteData exposing (WebData, succeed)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -9,3 +10,29 @@ update msg model =
     case msg of
         OnFetchObjects response ->
             ( { model | objects = response }, Cmd.none )
+
+        OrderObjects newOrdering ->
+            case model.objects of
+                RemoteData.NotAsked ->
+                    ( model, Cmd.none )
+
+                RemoteData.Loading ->
+                    ( model, Cmd.none )
+
+                RemoteData.Success objects ->
+                    let
+                        newModel =
+                            case newOrdering.field of
+                                Name ->
+                                    if newOrdering.order == Ascending then
+                                        { model | ordering = newOrdering, objects = succeed (List.sortBy .name objects) }
+                                    else
+                                        { model | ordering = newOrdering, objects = succeed (List.reverse (List.sortBy .name objects)) }
+
+                                Modified ->
+                                    { model | ordering = newOrdering, objects = succeed (List.sortBy .modified objects) }
+                    in
+                        ( newModel, Cmd.none )
+
+                RemoteData.Failure error ->
+                    ( model, Cmd.none )
