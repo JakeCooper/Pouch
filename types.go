7ad5a92d7fb36591fb9c20eb8a453e65b5f367b9
Jake Cooper <jake.elijah.cooper@gmail.com>
2017-09-16 01:23:51 -0400

    Basic interfaces setup

--- types.go
+++ types.go
@@ -1,8 +1,23 @@
 package main
 
+import "net/http"
+
 // CloudStorage : Interface for CloudStorage
 type CloudStorage interface {
-	Create()
-	Update()
-	Delete()
+	create() HTTPStatus
+	read() HTTPStatus
+	update() HTTPStatus
+	delete() HTTPStatus
+}
+
+// HTTPStatus : Status for HTTP, just an int
+type HTTPStatus int
+
+// AWS : Struct for Amazon Web Services
+type AWS struct {
+	Creds string
+}
+
+func (aws AWS) create() HTTPStatus {
+	return http.StatusOK
 }
