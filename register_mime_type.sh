6a072dd8210a5a75b4a40bc36b6248c6298283d8
Andrei Taylor <andreirtaylor@gmail.com>
2017-09-17 03:16:52 -0400

    Trying to fix mimes

--- register_mime_type.sh
+++ register_mime_type.sh
@@ -19,7 +19,7 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
 # Create application desktop
 echo "[Desktop Entry]
 Name=$APP
-Exec=Pouch %U
+Exec=Pouch %n
 MimeType=application/x-$APP
 Icon=/home/andrei/.local/share/icons/hicolor/48x48/apps/pouch.png
 Terminal=false
