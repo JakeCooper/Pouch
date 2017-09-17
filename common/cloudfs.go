842b2214d8c8e905c1561f44267e753578cd63c1
Jake Cooper <jake.elijah.cooper@gmail.com>
2017-09-16 20:39:10 -0400

    Recursion BABYYYYYY

--- common/cloudfs.go
+++ common/cloudfs.go
@@ -6,8 +6,6 @@ type WatcherObj struct {
 	Action string
 }
 
-var watcherChan = make(chan WatcherObj)
-
 // InitCloudFS returns an S3 interface
 func InitCloudFS(config *Configuration) (CloudStorage, error) {
 	return nil, nil
